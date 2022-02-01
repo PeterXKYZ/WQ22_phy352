79fd13f3154752b06c4ca5bfbcca959b9a623f75
PeterXKYZ <37281634+PeterXKYZ@users.noreply.github.com>
2022-02-01 14:37:05 -0600

    changes to rk engine

--- homework_3/ode/src/rk_gen.c
+++ homework_3/ode/src/rk_gen.c
@@ -4,6 +4,13 @@
 #include <constructors.h>
 #include <rk_gen.h>
 
+static void constant_multiply(double* dst, const double* a, double s, int length) {
+    for (int i = 0; i < length; ++i) {
+        dst[i] = a[i] * s;
+    }
+}
+
+/*
 static double* constant_multiply(const double* a, double s, int length) {
     double* dst = calloc(length, sizeof(double));
     if (!dst) {
@@ -16,7 +23,7 @@ static double* constant_multiply(const double* a, double s, int length) {
     }
 
     return dst;
-}
+}*/
 
 static void dot_add(double* a, const double* b, int length) {
     for (int i = 0; i < length; ++i) {
@@ -30,6 +37,8 @@ void ode_solver(double** x_2D, double* t,
                 ButcherTableau method, int num_var) {
     
     double k[method.order][num_var];
+    double want[num_var];
+    double storage[num_var];
 
     for (int i = 0; i < steps-1; ++i) {
 
@@ -38,24 +47,21 @@ void ode_solver(double** x_2D, double* t,
         }
         
         for (int j = 0; j < method.order; ++j) {
-            double want[num_var];
             for (int m = 0; m < num_var; ++m) {
                 want[m] = x_2D[i][m];
             }
 
             for (int l = 0; l < j; ++l) {
-                double* temp1 = constant_multiply(k[l], dt * method.matrix[j][l], num_var); 
-                dot_add(want, temp1, num_var);
-                free(temp1);
+                constant_multiply(storage, k[l], dt * method.matrix[j][l], num_var); 
+                dot_add(want, storage, num_var);
             }
 
             for (int n = 0; n < num_var; ++n) {
                 k[j][n] = dxdt[n](want, t[i] + method.nodes[j]*dt, dxdt_param);
             }
             
-            double* temp2 = constant_multiply(k[j], dt * method.weights[j], num_var);
-            dot_add(x_2D[i+1], temp2, num_var);
-            free(temp2);
+            constant_multiply(storage, k[j], dt * method.weights[j], num_var);
+            dot_add(x_2D[i+1], storage, num_var);
         }
         
         t[i+1] = t[i] + dt;
---
+++ homework_3/src/assignment3_4.c
@@ -0,0 +1,101 @@
+#include <assignment3_4_physics.h>
+#include <rk_deriv.h>
+#include <stdio.h>
+
+#define MAX_TIME 751
+#define NUM_VAR 2
+#define OMEGA_POINTS 71
+#define Q_POINTS 41
+
+int main(void) {
+    // double tic = clock();   // how fast does this code run?
+
+    double (*func[NUM_VAR]) (double*, double, double*);
+    func[0] = dtheta_dt;
+    func[1] = domega_dt;
+
+    double dt = .2;
+
+    // range of omega values to scan over
+    double omega_arr[OMEGA_POINTS];
+    omega_arr[0] = .667;
+    for (int i = 1; i < OMEGA_POINTS-1; i += 2) {
+        omega_arr[i] = omega_arr[0] + .01*i;
+        omega_arr[i+1] = omega_arr[0] - .01*i; 
+    }
+
+    // range of q values to scan over
+    double q_arr[Q_POINTS];
+    for (int i = 0; i < Q_POINTS; i++) {
+        q_arr[i] = pow(10, -1 + .05*i);
+    } 
+
+    // param[0] = sqrt(g/l) = Omega 
+    // param[1] = q
+    // param[2] = F_d
+    // param[3] = Omega_d
+    double param[] = {.667, .3, .2, .667};
+
+    // x[0] = theta, x[1] = omega
+    // the driving force will make it move
+    double x_init[] = {0, 0};
+
+    // constructing the x_2D and t array
+    double* t = t_constructor(MAX_TIME, 0);
+    double** x = x_constructor(NUM_VAR, MAX_TIME, x_init);
+
+    // make a 1D array to store just the angle information
+    double angles[MAX_TIME];
+
+    // ----------------------------scan over Omega_d----------------------------------------
+
+    FILE* data1 = fopen("data/assignment3_4_data/assignment3_4_Omega.dat", "w");
+
+    for (int i = 0; i < OMEGA_POINTS; ++i) {
+        param[3] = omega_arr[i]; // set Omega_d to one of the values in array
+        rkO4(x, t, func, param, dt, MAX_TIME, NUM_VAR);
+
+        for (int j = 0; j < MAX_TIME; ++j) {
+            angles[j] = x[j][0];
+        }
+
+        int ind = find_extremum(angles, MAX_TIME); // find the steady-state amplitude
+
+        fprintf(data1, "%lf %lf\n", omega_arr[i], fabs(angles[ind])); // fabs = float absolute value
+    }
+
+    fclose(data1);
+    
+    // ---------------------------scan over q----------------------------------------------
+
+    FILE* data2 = fopen("data/assignment3_4_data/assignment3_4_q.dat", "w");
+    param[3] = .667;    // set Omega_d back to resonance freq
+
+    for (int i = 0; i < Q_POINTS; ++i) {
+        param[1] = q_arr[i];
+        rkO4(x, t, func, param, dt, MAX_TIME, NUM_VAR);
+
+        for (int j = 0; j < MAX_TIME; ++j) {
+            angles[j] = x[j][0];
+        }
+
+        int ind = find_extremum(angles, MAX_TIME);
+
+        fprintf(data2, "%lf %lf\n", q_arr[i], fabs(angles[ind]));
+    }
+
+    fclose(data2);
+
+    // ----------------------destroy everything------------------------------
+
+    t_destroyer(t);
+    x_destroyer(x, MAX_TIME);
+
+    /*
+    double toc = clock();
+    double time = (double)(toc - tic) / CLOCKS_PER_SEC;
+    printf("%lf", time);
+    */
+
+    return 0;
+}
