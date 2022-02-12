4524484810369b6314fedb817cb223ec1c5a8774
PeterXKYZ <37281634+PeterXKYZ@users.noreply.github.com>
2022-02-12 15:59:40 -0600

    uncommented

--- midterm/Linux/src/midterm_2_main.c
+++ midterm/Linux/src/midterm_2_main.c
@@ -44,28 +44,28 @@ int main(int argc, char** argv) {
     FILE* data = fopen(argv[2], "w");
 
     // preparing fft
-    // double* in = (double*) fftw_malloc(sizeof(double) * tlen);
-	// fftw_complex* out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * tlen);
-	// fftw_plan p = fftw_plan_dft_r2c_1d(tlen, in, out, FFTW_ESTIMATE);
+    double* in = (double*) fftw_malloc(sizeof(double) * tlen);
+    fftw_complex* out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * tlen);
+	fftw_plan p = fftw_plan_dft_r2c_1d(tlen, in, out, FFTW_ESTIMATE);
     
-    // for (int n = 0; n < tlen; ++n) {
-        // in[n] = y_2D[n][5];
-    // }
+    for (int n = 0; n < tlen; ++n) {
+        in[n] = y_2D[n][5];
+    }
 
-    // fftw_execute(p);
+    fftw_execute(p);
 
     for (int n = 0; n < tlen; ++n) {
         if (!(fabs(y_2D[n][5]) > .001)) {
             continue;
         }
         fprintf(data, "t: %lf a: %lf f: %lf\n", n*dt, y_2D[n][5], n/(2*pi*tlen*dt));
-        // fprint(data, "re: %lf im %lf\n", out[n][0], out[n][1]);
-        // in[n] = y_2D[n][5];
+        fprint(data, "re: %lf im %lf\n", out[n][0], out[n][1]);
+        in[n] = y_2D[n][5];
     }
     
-    // fftw_destroy_plan(p);
-	// fftw_free(in);
-	// fftw_free(out);
+    fftw_destroy_plan(p);
+	fftw_free(in);
+	fftw_free(out);
     arr_2D_destroyer(y_2D, tlen);
     return 0;
 }
