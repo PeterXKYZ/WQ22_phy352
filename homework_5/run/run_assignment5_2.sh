af65b56b35f90e3dfbcdac2e75a5ae8c20a4561a
PeterXKYZ <ghastyz@gmail.com>
2022-03-04 18:09:26 -0600

    logs

---
+++ homework_5/run/run_assignment5_2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+./bin/assignment5_2a.exe >& data/logs/assignment5_2a.log
+
+./bin/assignment5_2b.exe >& data/logs/assignment5_2b.log
+
+./bin/assignment5_2c.exe >& data/logs/assignment5_2c.log
