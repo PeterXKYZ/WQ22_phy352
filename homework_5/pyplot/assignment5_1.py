cfacf08517fa763dfa1c9f5631115481fd3ae1f9
PeterXKYZ <37281634+PeterXKYZ@users.noreply.github.com>
2022-03-04 19:25:18 -0600

    last push

--- homework_5/pyplot/assignment5_1.py
+++ homework_5/pyplot/assignment5_1.py
@@ -1,13 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-_, F_D, _, theta = np.genfromtxt("data/assignment5_1_data/bifurcation.dat", unpack=True, dtype=float)
+_, F_D, _, theta = np.genfromtxt("../data/assignment5_1_data/bifurcation.dat", unpack=True, dtype=float)
 
 fig, ax = plt.subplots()
 
-
 ax.scatter(F_D, theta, s=.5, color="red")
 ax.set(xlabel=r"$F_D$", ylabel=r"$\theta$")
+ax.set_title("Bifurcation Diagram")
 
-plt.tight_layout()
-plt.show()
+fig.tight_layout()
+fig.savefig("../graphics/bifurcation.png")
+
+# --------------------------------------------
+fig2, ax2 = plt.subplots()
+
+ax2.scatter(F_D, theta, s=.5, color="red")
+ax2.set(xlabel=r"$F_D$", ylabel=r"$\theta$", xlim=(1.4575, 1.4892), ylim=(1.91, 3.11),
+        title="Bifurcation Diagram Zoomed")
+
+fig2.tight_layout()
+fig.savefig("../graphics/bifur_zoomed.png")
---
+++ homework_5/readme.txt
@@ -0,0 +1,11 @@
+Problem 1: 
+Let F_n = 1.476, F_n+1 = 1.479, F_n-1 = 1.4595.
+The Feigenbaum constant δ is (F_n - F_n-1) / (F_n+1 - F_n) = 5.5
+which is uh, a little bit off.
+
+TECHNICAL COMMENTS:
+- the plots are generated with Python's Matplotlib cuz I'm running out of time and didn't want 
+to fiddle with gnuplot.
+- the Python scripts may not be compatible with an outdated Matplotlib package on Quest. (Works with Mpl 3.4.2)
+- the Python scripts should be run in the pyplot/ folder.
+- since Problem 2 has many subparts, I split up the problem 2 into three mains. 
